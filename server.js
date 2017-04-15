00b17b314073cb11abb64f8ae2b470ce268a0f7a
Dakota <bazookaharwell@gmail.com>
2017-04-14 19:44:09 -1000

    Minor: Fix indentation

--- server.js
+++ server.js
@@ -7,52 +7,52 @@ const headers = [];
 
 const server = http.createServer( (req, res) => {
 
-    const method = req.method;
-    let path = req.url;
-    const httpVersion = 'HTTP' + req.httpVersion;
+  const method = req.method;
+  let path = req.url;
+  const httpVersion = 'HTTP' + req.httpVersion;
 
-    path = 'public' + path;
+  path = 'public' + path;
 
-    if (path === 'public/') {
-      path = 'public/index.html';
-    }
+  if (path === 'public/') {
+    path = 'public/index.html';
+  }
 
-    let response = null;
+  let response = null;
 
-    if (method === "GET") {
-      generateResponse(path);
-    }
+  if (method === "GET") {
+    generateResponse(path);
+  }
 
-    function generateResponse(file) {
-      var fileData = '';
-      return fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-          console.log(err);
-          return notFound();
-        }
-        sendResponse(data);
-      });
-    }
+  function generateResponse(file) {
+    var fileData = '';
+    return fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+        return notFound();
+      }
+      sendResponse(data);
+    });
+  }
 
-    function notFound() {
-      fs.readFile('public/404.html', 'utf8', (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        sendResponse(data, 404);
-      });
-    }
+  function notFound() {
+    fs.readFile('public/404.html', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      sendResponse(data, 404);
+    });
+  }
 
-    function sendResponse(data, status = 200) {
-      res.writeHead(status, {
-        'Content-Type'  : 'text/html',
-        'Content-Length': data.length,
-        'Date'          : new Date().toUTCString(),
-        'Server'        : 'HackerSpace'
-      });
-      res.write(`${data}`);
-      res.end();
-    }
+  function sendResponse(data, status = 200) {
+    res.writeHead(status, {
+      'Content-Type'  : 'text/html',
+      'Content-Length': data.length,
+      'Date'          : new Date().toUTCString(),
+      'Server'        : 'HackerSpace'
+    });
+    res.write(`${data}`);
+    res.end();
+  }
 });
 
 server.listen(8080);
