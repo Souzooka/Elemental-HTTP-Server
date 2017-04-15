b457233da9c52dc5e69ee69297c37e8d65a30cc3
Dakota <bazookaharwell@gmail.com>
2017-04-15 12:21:40 -1000

    Parse incoming bodies

--- server.js
+++ server.js
@@ -2,18 +2,19 @@
 const net = require('net');
 const fs = require('fs');
 const http = require('http');
+const querystring = require('querystring');
 
 const headers = [];
 const server = http.createServer( (req, res) => {
 
-  function generateResponse(file) {
+  function generateGETResponse(file) {
     var fileData = '';
     return fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
         console.log(err);
         notFound();
       } else {
-        sendResponse(data);
+        sendGETResponse(data);
       }
     });
   }
@@ -23,11 +24,11 @@ const server = http.createServer( (req, res) => {
       if (err) {
         console.log(err);
       }
-      sendResponse(data, 404);
+      sendGETResponse(data, 404);
     });
   }
 
-  function sendResponse(data, status = 200) {
+  function sendGETResponse(data, status = 200) {
     res.writeHead(status, {
       'Content-Type'  : 'text/html',
       'Content-Length': data.length,
@@ -38,26 +39,48 @@ const server = http.createServer( (req, res) => {
     res.end();
   }
 
+  function createFile(filepath, data) {
+
+  }
+
   const method = req.method;
   const httpVersion = 'HTTP' + req.httpVersion;
+  var body = [];
+
+
   let path = 'public' + req.url;
 
   if (path === 'public/') {
     path = 'public/index.html';
   }
 
-  switch (method) {
-    case 'GET': {
-      generateResponse(path);
-      break;
+  req.on('error', function(err) {
+    console.error(err);
+  });
+
+  req.on('data', function(chunk) {
+    body.push(chunk);
+  });
+
+  req.on('end', () => {
+    body = querystring.parse(Buffer.concat(body).toString());
+    switch (method) {
+      case 'GET': {
+        generateGETResponse(path);
+        break;
+      }
+      case 'POST': {
+        console.log(body);
+        break;
+      }
+      case 'OPTIONS': {
+        res.writeHead(200);
+        res.write('GET, POST, PUT, DELETE, HEADERS');
+        res.end();
+        break;
+      }
     }
-    case 'OPTIONS': {
-      res.writeHead(200);
-      res.write('GET, POST, PUT, DELETE, HEADERS');
-      res.end();
-      break;
-    }
-  }
+  });
 });
 
 server.listen(8080);
